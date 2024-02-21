ae9e72e904a13ea925e1df5c7e03e9ea7dbe57f2
alice7lima <alice7252011@gmail.com>
2024-02-20 22:12:11 -0300

    Adicionando a implementação da aula 1.

---
+++ aula01/kpi.py
@@ -0,0 +1,11 @@
+BONUS_2024 = 1000
+
+nome = input("Bem vind@! Digite o seu nome: ")
+
+salario = float(input("Digite o seu salário: "))
+
+bonus = float(input("Digite o seu bônus: "))
+
+kpi_bonus = BONUS_2024 + salario * bonus
+
+print(f"O usuário {nome} possui o bonus de {kpi_bonus}")
