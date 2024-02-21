db283a14a1cc30d6c2e0eee33dd2ed59e6c1eb89
alice7lima <alice7252011@gmail.com>
2024-02-20 22:39:01 -0300

    Adicionando funcionalidade na resolução da aula 1.

--- aula01/kpi.py
+++ aula01/kpi.py
@@ -9,3 +9,5 @@ bonus = float(input("Digite o seu bônus: "))
 kpi_bonus = BONUS_2024 + salario * bonus
 
 print(f"O usuário {nome} possui o bonus de {kpi_bonus}")
+
+print(f"O salário do usuário {nome} é R$ {salario} e o bonus é de {bonus}")
