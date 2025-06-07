84a29a772cd25f47161cb27e9b42b6669c96850e
alice7lima <alice7252011@gmail.com>
2025-06-07 18:00:28 -0300

    adiciona projeto aula 8

---
+++ aula08/etl.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import os, glob
+
+
+def extrair_dados(diretorio_dados: str, formato: str) -> pd.DataFrame:
+
+    if formato not in ["json", "csv", "xlsx"]:
+        raise Exception(
+            "Formato dos arquivos de entrada não suportado! Formatos válidos: json, csv e xlsx (excel)"
+        )
+
+    arquivos = glob.glob(os.path.join(diretorio_dados, f"*.{formato}"))
+
+    funcao_pandas = {"csv": pd.read_csv, "json": pd.read_json, "xlsx": pd.read_excel}
+
+    if len(arquivos) > 0:
+
+        df_lista = [funcao_pandas[formato](arquivo) for arquivo in arquivos]
+        df_total = pd.concat(df_lista, ignore_index=True)
+
+        return df_total
+
+    else:
+        raise Exception(
+            "O diretório informado está vazio, verifique e tente novamente."
+        )
+
+
+def transformar_dados(df: pd.DataFrame) -> pd.DataFrame:
+    df["Receita"] = df["Quantidade"] * df["Venda"]
+    print(df.head())
+
+    return df
+
+
+def carregar_dados(df: pd.DataFrame, formatos: list) -> None:
+    for formato in formatos:
+        if formato == "csv":
+            df.to_csv("dados.csv", index=False)
+
+        elif formato == "parquet":
+            df.to_parquet("dados.parquet", index=False)
+
+
+def pipeline(diretorio_entrada: str, formato_entrada: str, formato_saida: str) -> None:
+    dados = extrair_dados(diretorio_entrada, formato=formato_entrada)
+    dados_transformados = transformar_dados(dados)
+    carregar_dados(dados_transformados, formato_saida)
---
+++ aula08/pipeline.py
@@ -0,0 +1,14 @@
+from etl import pipeline
+from pathlib import Path
+
+if __name__ == "__main__":
+    diretorio_raiz = Path(__file__).parent
+    diretorio_entrada = diretorio_raiz / "data"
+    formato_entrada = "json"
+    formato_saida = ["parquet"]
+
+    pipeline(
+        diretorio_entrada=diretorio_entrada,
+        formato_entrada=formato_entrada,
+        formato_saida=formato_saida,
+    )
